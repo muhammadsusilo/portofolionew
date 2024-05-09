c694eccfeb49adcc948c418cad5599f925d83ab5
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-09 21:17:13 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom'
+import './index.css';
+import Navbar from "./Fragments/Navbar";
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<h1>Hello React</h1>,
+  },
+  {
+    path:"Navbar",
+    element:< Navbar/>
+  },
+
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
