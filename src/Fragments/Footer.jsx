0b30c5390fd647457a3fde2ef2389b35afe3d894
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-19 16:15:39 +0700

    create template footer

--- src/Fragments/Footer.jsx
+++ src/Fragments/Footer.jsx
@@ -1,11 +1,23 @@
-import SosialMedia from "../Elements/sosialMedia";
+import SosialMedia from '../Elements/sosialMedia';
 
 const Footer = () => {
-    return (
-        <div>
-            < SosialMedia/>
-        </div>
-    )
-}
+  return (
+    <div className="py-10 bg-gray-100 flex justify-center flex-col items-center text-center leading-loose">
+      <h1>Muhammad Susilo</h1>
+      <ul className="flex gap-4 mb-2 ">
+        <li>Home</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Certificate</li>
+        <li>Contact</li>
+      </ul>
+      <SosialMedia />
+      <div>
+        <p>Copyright &copy; 2021 Muhammad Susilo. All rights reserved.</p>
+        <p>in created to React JS and Tailwind CSS</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,49 +1,56 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faIdCard } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import Footer from '../Fragments/Footer';
 
 const Contact = () => {
   return (
-    <div className="px-36 py-20 h-full bg-gray-300">
-      <h1 className="text-3xl font-semibold flex justify-center mb-4">
-        Contact
-      </h1>
-      <div className='bg'>
-        <h2 className="my-1 font-semibold text-lg">Contact information</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <ul className="leading-normal">
-          {/* untuk ke depan yang tulisan di a seperti email, linkedin, whatsapp, kartu nama bisa langsung  di tulis nama email, dll */}
-          <li>
-            <a href="#">
-              <FontAwesomeIcon icon={faEnvelope} className="mr-3 text-xl" />
-              Email
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FontAwesomeIcon icon={faLinkedin} className="mr-3 text-xl" />
-              Linkedin
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FontAwesomeIcon icon={faWhatsapp} className="mr-3 text-xl" />
-              WhatsApp
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FontAwesomeIcon icon={faIdCard} className="mr-3 text-xl" />
-              Bussiness Card
-            </a>
-          </li>
-        </ul>
-        <a href="#" className="w-fit">
-          <button className="my-5 p-2 px-16 bg-orange-400 rounded-lg hover:bg-orange-500"> Download CV</button>
-        </a>
+    <>
+      <div className="px-36 py-20 h-full bg-gray-300">
+        <h1 className="text-3xl font-semibold flex justify-center mb-4">
+          Contact
+        </h1>
+        <div className="bg">
+          <h2 className="my-1 font-semibold text-lg">Contact information</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <ul className="leading-normal">
+            {/* untuk ke depan yang tulisan di a seperti email, linkedin, whatsapp, kartu nama bisa langsung  di tulis nama email, dll */}
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faEnvelope} className="mr-3 text-xl" />
+                Email
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faLinkedin} className="mr-3 text-xl" />
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faWhatsapp} className="mr-3 text-xl" />
+                WhatsApp
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faIdCard} className="mr-3 text-xl" />
+                Bussiness Card
+              </a>
+            </li>
+          </ul>
+          <a href="#" className="w-fit">
+            <button className="my-5 p-2 px-16 bg-orange-400 rounded-lg hover:bg-orange-500">
+              {' '}
+              Download CV
+            </button>
+          </a>
+        </div>
+        <div>{/* nanti disini form untuk masuk ke email */}</div>
       </div>
-      <div>{/* nanti disini form untuk masuk ke email */}</div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
