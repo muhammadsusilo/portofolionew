617325bd39645ced76ce6f8bacc96be84ac031dd
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-19 14:10:24 +0700

    adding kalimat di section home and about,

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,29 +1,28 @@
 const About = () => {
   return (
-    <div>
-      <div className="px-36 py-20 h-screen flex justify-between">
-        <div className="">
+    <div className="px-36 ">
+      <h1 className="text-3xl font-semibold py-5 flex justify-center">About</h1>
+      <div className="h-screen flex justify-between">
+        <div>
           <img src="https://placehold.co/500x500?font=roboto" alt="About" />
         </div>
         <div className="pl-10">
-          <h1 className="text-2xl font-semibold">About</h1>
           <p className="my-1">
-            Muhammad Susilo call me silo is ambiton to career Software
-            Developer. And undergraduate Terbuka University with major system
+            Hi I, Muhammad Susilo call me silo is ambiton to career Software
+            Developer. And undergraduate in Terbuka University with major system
             Information.
           </p>
           <p>
-            <span className="font-semibold mb-1 text-xl"> Skills</span> <br />
+            <span className="font-semibold"> Skills</span> <br />
             Skills silo yang dimiliki:
-            <ul className="">
-              <li>HTML5 & CSS3</li>
-              <li>SASS</li>
-              <li>Tailwind CSS</li>
-              <li>JavaScript</li>
-              <li>React JS</li>
-              {/* <li>PHP</li> */}
-            </ul>
           </p>
+          <ul className="mt-3 list-disc pl-6 text-sm">
+            <li>HTML5 & CSS3</li>
+            <li>SASS</li>
+            <li>Tailwind CSS</li>
+            <li>JavaScript</li>
+            <li>React JS</li>
+          </ul>
         </div>
       </div>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,27 +9,26 @@ const Home = () => {
         <Navbar />
         <div className=" ">
           <div className="p-36 flex h-screen bg-gray-200 justify-between">
-            <div className=" mr-10">
-              <h1 className="font-semibold text-2xl">Hello I</h1>
+            <div className=" w-2/3">
+              <h1 className="font-semibold text-2xl">Hello I'm</h1>
               <h5 className="font-semibold text-2xl">Muhammad Susilo</h5>
               <h1 className="text-3xl	">
                 Software Developer || FrontEnd Developer
               </h1>
               <p className="my-2 text-base">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                distinctio ullam obcaecati deserunt culpa voluptate eius modi
-                non omnis sunt recusandae
+                Hello, welcome to personal website, <br />
+                Scroll to view my portofolio
               </p>
               <div className="font-semibold my-8 flex group:">
                 <div className="group mr-4 ">
-                  <a href="">
+                  <a href="#">
                     <button className="group-hover:translate-y-6 bg-orange-400 hover:bg-orange-600 transition duration-200 ease-in  p-3 rounded-md">
                       Hire Me
                     </button>
                   </a>
                 </div>
-                <div className="group ">
-                  <a href="">
+                <div className="group">
+                  <a href="#">
                     <button className="group-hover:translate-x-6 bg-orange-400 hover:bg-orange-600 transition duration-200 ease-in  p-3 rounded-md">
                       Download CV
                     </button>
@@ -38,9 +37,9 @@ const Home = () => {
               </div>
               <SosialMedia />
             </div>
-            <div>
+            <div className="w-1/3">
               <img
-                src="https://placehold.co/500x500?font=roboto"
+                src="https://placehold.co/400x400?font=roboto"
                 alt="Muhammad Susilo"
                 className="ml-10"
               />
