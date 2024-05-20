e9a877a593fb83545840223e6334508c9850c099
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-20 15:14:19 +0700

    rendering list di footer

--- src/Fragments/Footer.jsx
+++ src/Fragments/Footer.jsx
@@ -1,15 +1,23 @@
 import SosialMedia from '../Elements/sosialMedia';
 
+const listFooter = [
+  { id: 1, link: '#', name: 'Home'},
+  {id: 2,link: '#', name: 'About'},
+  {  id: 3, link: '#', name: 'Project'},
+  { id: 4, link: '#',  name: 'Certificate'},
+  {  id: 5,link: '#', name: 'Contact'},
+];
+
 const Footer = () => {
   return (
     <div className="py-10 bg-gray-100 flex justify-center flex-col items-center text-center leading-loose">
       <h1>Muhammad Susilo</h1>
       <ul className="flex gap-4 mb-2 ">
-        <li>Home</li>
-        <li>About</li>
-        <li>Project</li>
-        <li>Certificate</li>
-        <li>Contact</li>
+       {listFooter.map((list) =>{
+         <li key={list.id}>
+           <a href={list.link}>{list.name}</a>
+         </li>;
+       })}
       </ul>
       <SosialMedia />
       <div>
