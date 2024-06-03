e1fe3e34ae4a91fc54c4dbca1a30063fb91dc032
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-06-04 06:56:21 +0700

    revisi section sosial media

--- src/Elements/sosialMedia.jsx
+++ src/Elements/sosialMedia.jsx
@@ -16,25 +16,28 @@ const sMedia = [
   {
     id: 1,
     name: 'Email',
-    href: '#',
+    href: 'mailto:muhammadsusilo238@gmail.com',
     ikon: faEnvelope,
+    target: '_blank',
   },
   {
     id: 2,
     name: 'Github',
     href: 'https://github.com/muhammadsusilo',
     ikon: faGithub,
+    target: '_blank',
   },
   {
     id: 3,
     name: 'Linkedin',
     href: 'https://www.linkedin.com/in/muhammadsusilo/',
     ikon: faLinkedin,
+    target: '_blank',
   },
   {
     id: 4,
     name: 'WhatsApp',
-    href: '#',
+    href: 'https://api.whatsapp.com/send?phone=%20085642045753&text=Halo%2C%20Muhamad%20Susilo',
     ikon: faWhatsapp,
   },
 ];
@@ -43,7 +46,7 @@ const SosialMedia = () => {
     <div className="w-fit mx-3 my-3 flex animate__animated animate__backInUp animate__slow max-sm:justify-center max-sm:items-center max-sm:w-auto">
       {sMedia.map((media) => (
         <div className="text-center group" key={media.id}>
-          <Link to={media.href}>
+          <Link to={media.href} target={media.target}>
             <FontAwesomeIcon
               icon={media.ikon}
               className="bg-gray-300 rounded-full p-3 mx-2 size-6 transition-all duration-200 group-hover:scale-150 active:bg-violet-500 active:text-white"
